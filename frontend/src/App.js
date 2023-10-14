c62b0c15f3111eecfd0516dcb188ea88ca436526
mansi <mansi@192.168.1.6>
2023-10-14 13:15:49 +0530

    login base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,57 @@
+// import { useState } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Topbar from "./scenes/global/Topbar";
+// import Sidebar from "./scenes/global/Sidebar";
+// import Dashboard from "./scenes/dashboard";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Form2 from "./scenes/fSideorm2";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { ColorModeContext, useMode } from "./theme";
+// import Calendar from "./scenes/calendar/calendar";
+// import Signin from "./scenes/signin/signin";
+// import Signup from "./scenes/signup";
+// function App() {
+//   const [theme, colorMode] = useMode();
+//   const [isSidebar, setIsSidebar] = useState(true);
+
+//   return (
+//     <ColorModeContext.Provider value={colorMode}>
+//       <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <div className="app">
+//           <Sidebar isSidebar={isSidebar} />
+//           <main className="content">
+//             <Topbar setIsSidebar={setIsSidebar} />
+//             <Routes>
+//               <Route path="/signin" element={<Signin/>}/>
+//               <Route path="/signup" element={<Signup/>}/>
+//               <Route path="/" element={<Dashboard />} />
+//               <Route path="/contacts" element={<Contacts />} />
+//               <Route path="/invoices" element={<Invoices />} />
+//               <Route path="/form" element={<Form />} />
+//               <Route path="/form2" element={<Form2/>} />
+//               <Route path="/bar" element={<Bar />} />
+//               <Route path="/pie" element={<Pie />} />
+//               <Route path="/line" element={<Line />} />
+//               <Route path="/faq" element={<FAQ />} />
+//               <Route path="/calendar" element={<Calendar />} />
+//             </Routes>
+//           </main>
+//         </div>
+//       </ThemeProvider>
+//     </ColorModeContext.Provider>
+//   );
+// }
+
+// export default App;
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
@@ -14,25 +66,35 @@ import FAQ from "./scenes/faq";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Calendar from "./scenes/calendar/calendar";
+import Signin from "./scenes/signin/signin";
+import Signup from "./scenes/signup";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Check if the current route is signin or signup
+  const isSignInOrSignUp = location.pathname === "/signin" || location.pathname === "/signup";
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          {!isSignInOrSignUp && <Sidebar isSidebar={isSidebar} />}
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            {!isSignInOrSignUp && <Topbar setIsSidebar={setIsSidebar} />}
             <Routes>
+              <Route path="/signin" element={<Signin />}/>
+              <Route path="/signup" element={<Signup />}/>
               <Route path="/" element={<Dashboard />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              <Route path="/form2" element={<Form2/>} />
+              <Route path="/form2" element={<Form2 />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
